062f669d55e6e10de87ab0aeb4834f1ce83e430e
Vineet Malewar <63373736+vintu2001@users.noreply.github.com>
2025-11-09 21:11:11 -0800

    CORS fix (#11)

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,  # Changed to False for compatibility with allow_origins=["*"]
     allow_methods=["*"],
     allow_headers=["*"],
 )
